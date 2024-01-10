d228bb30bc37ecc4661f28488927da4a42103d7f
odootest <odootest2023@163.com>
2024-01-10 17:06:54 +0800

    init modules

--- odoo/modules/module.py
+++ odoo/modules/module.py
@@ -1,3 +1,11 @@
 
+import sys
+
 def load_openerp_module(module_name):
     print('load_openerp_module', module_name)
+
+    qualname = f'odoo.addons.{module_name}'
+    if qualname in sys.modules:
+        return
+
+    __import__(qualname)
