b53f8a8084907263956f303cbde6bc64847f412e
odootest <odootest2023@163.com>
2024-01-10 13:44:13 +0800

    tools/config.py

--- odoo/cli/server.py
+++ odoo/cli/server.py
@@ -1,4 +1,8 @@
 
+import sys
+from pathlib import Path
+
+import odoo
 from .command import Command
 
 def main(args):
@@ -6,4 +10,5 @@ def main(args):
 
 class Server(Command):
     def run(self, args):
+        odoo.tools.config.parser.prog = f'{Path(sys.argv[0]).name} {self.name}'
         main(args)
---
+++ odoo/tools/config.py
@@ -0,0 +1,8 @@
+
+import optparse
+
+class configmanager(object):
+    def __init__(self, fname=None):
+        self.parser = optparse.OptionParser()
+
+config = configmanager()
